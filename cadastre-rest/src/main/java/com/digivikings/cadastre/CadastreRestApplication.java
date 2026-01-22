24343712b9fad0859ce087b18e8fccd39c4fc153
AndreiKoshelap <Andrei.Koshelap@digivikings.com>
2026-01-22 11:23:38 +0200

    initialize adapter service with Spring Boot setup and Docker configuration

---
+++ cadastre-rest/src/main/java/com/digivikings/cadastre/CadastreRestApplication.java
@@ -0,0 +1,13 @@
+package com.digivikings.cadastre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastreRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastreRestApplication.class, args);
+	}
+
+}
---
+++ cadastre-rest/src/test/java/com/digivikings/cadastre/CadastreRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digivikings.cadastre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastreRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
